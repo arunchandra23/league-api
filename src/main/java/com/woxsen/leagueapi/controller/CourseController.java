32f538bf77417ebfa3140a179fa2089aa0a91a25
Arunchandra Boini <arunchandra.boini@effigo.in>
2023-02-24 15:08:09 +0530

    fix space

--- src/main/java/com/woxsen/leagueapi/controller/CourseController.java
+++ src/main/java/com/woxsen/leagueapi/controller/CourseController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping(AppConstants.BASE_URL+"/branches ")
+@RequestMapping(AppConstants.BASE_URL+"/branches")
 public class CourseController {
 
     @Autowired
