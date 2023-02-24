551fa2301302f013bfb4ae11356bedd7c5210eeb
Arunchandra Boini <arunchandra.boini@effigo.in>
2023-02-24 11:01:34 +0530

    added component scan fix

--- src/main/java/com/woxsen/leagueapi/LeagueApiApplication.java
+++ src/main/java/com/woxsen/leagueapi/LeagueApiApplication.java
@@ -2,8 +2,10 @@ package com.woxsen.leagueapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.woxsen.leagueapi")
 public class LeagueApiApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/woxsen/leagueapi/security/SecurityConfig.java
+++ src/main/java/com/woxsen/leagueapi/security/SecurityConfig.java
@@ -51,6 +51,7 @@ public class SecurityConfig {
                 .configurationSource(corsConfigurationSource());
         return http.build();
     }
+    @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         List<String> allowOrigins = Arrays.asList("*");
